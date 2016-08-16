aa73a73ebc469b305b30974ea75e9741e93b5025
文志伟 <wenzhiwei@goopal.com>
2016-08-16 16:32:14 +0800

    update template

---
+++ App.java
@@ -0,0 +1,27 @@
+#{accesspackage};
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+//@ImportResource("classpath:dubbo.xml")
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Environment env = (Environment) ctx.getBean("environment");
+        System.out.println("端口号:" + env.getProperty("local.server.port"));
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
